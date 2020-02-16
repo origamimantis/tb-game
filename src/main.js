359b4ef9ad67f57f806dae147193b988c58ab2b6
eric zhang <ezhang323@gmail.com>
2020-02-15 17:58:10 -0800

    ii

--- src/main.js
+++ src/main.js
@@ -31,8 +31,8 @@ window.onload = () =>
     {
       MapPath : "assets/tilemaps/lvl3.txt",
       ImgLoad : [	"P_gen", "P_janitor",
-		  "T_field_tree_B", "T_field_tree_M", "T_field_tree_T", "T_5",
-		  "T_6", "T_7", "T_8", "T_wall_B", "T_wall_BR",
+		  "T_field_tree_B", "T_field_tree_M", "T_field_tree_T", 
+		   "T_wall_B", "T_wall_BR",
 		  "T_wall_EL", "T_wall_ER", "T_wall_TL", "T_wall_TR",
 		  "S_kn0", "S_lead1",
 		  "C_c0", "C_move"
